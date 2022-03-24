5ead3121e5662f4945e3bd59f4058d8a06d02bda
amigo <joewu590@gmail.com>
2022-03-24 10:59:33 +0800

    hide rBridge for now

--- packages/apps/src/pages/RBridge.tsx
+++ packages/apps/src/pages/RBridge.tsx
@@ -3,7 +3,7 @@ import { CardContainer } from "@stafihub/react-components";
 export const RBridge = () => {
   return (
     <div className="flex items-center pt-2 flex-col">
-      <div className="mb-6 h-[30px] px-10 rounded-full border-border-main border-solid border-[1px] flex justify-center items-center">
+      {/* <div className="mb-6 h-[30px] px-10 rounded-full border-border-main border-solid border-[1px] flex justify-center items-center">
         <div className="text-white text-[14px] font-bold">Get FIS from</div>
         <div className="ml-1 text-primary text-[14px] font-bold cursor-pointer">
           Fee Station
@@ -21,7 +21,7 @@ export const RBridge = () => {
       <div className="mt-4 text-text-gray5 text-[12px]">
         Reminder: You cannot directly swap tokens to Exchange address from
         rBridge UI.
-      </div>
+      </div> */}
     </div>
   );
 };

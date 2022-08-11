13b29899ee816f9477837afc74f51c1d6f5e44c6
amigo <joewu590@gmail.com>
2022-08-11 20:15:02 +0800

    refine coming soon page UI

--- packages/apps/src/components/ComingSoon.tsx
+++ packages/apps/src/components/ComingSoon.tsx
@@ -4,10 +4,14 @@ import { openLink } from "../utils/common";
 export const ComingSoon = () => {
   return (
     <div className="flex flex-col items-center">
-      <img src={comingSoon} className="w-[800px]" alt="coming soon" />
+      <img
+        src={comingSoon}
+        className="mt-[100px] w-[640px]"
+        alt="coming soon"
+      />
 
       <div
-        className="mt-[-100px] border-solid border-[1px] border-[#9DAFBE] rounded-full px-[80px] py-[20px] font-arialBold font-bold text-[24px] text-white flex items-center justify-center cursor-pointer tracking-wider"
+        className="mt-[-80px] border-solid border-[1px] border-[#9DAFBE] rounded-full px-[80px] py-[20px] font-arialBold font-bold text-[28px] text-white flex items-center justify-center cursor-pointer tracking-wider"
         onClick={() => openLink("https://discord.com/invite/jB77etn")}
       >
         Follow Us For Update

58f319ebda4aa201cc8cc8881c67c95c77c04706
joe <joe@stafi.io>
2023-03-10 16:59:26 +0800

    fix import warning

--- install-dependency.sh
+++ install-dependency.sh
@@ -1,1 +1,7 @@
-rm -rf node_modules && rm -rf packages/app/node_modules && rm -rf packages/react-components/node_modules && pnpm i
+rm -rf node_modules && rm -rf packages/apps/node_modules \ 
+&& rm -rf packages/react-components/node_modules \
+&& rm -rf packages/apps-wallet/node_modules \
+&& rm -rf packages/apps-util/node_modules \
+&& rm -rf packages/apps-config/node_modules \
+&& rm -rf packages/types/node_modules \
+&& pnpm i
